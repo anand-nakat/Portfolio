b5edef46a462c6ce5ea506d6e1aafa9e1898c447
Anand Nakat <60821265+anand-nakat@users.noreply.github.com>
2023-01-29 13:43:59 +0530

    Image import fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Sidebar from "../components/Sidebar";
-import { myself } from "../images/myself.jpg";
+import myself  from "../images/myself.jpg";
 import Modal from "../components/Modal";
 import { dateObject } from "../utilities/Day-Months";
 import { useGlobalContext } from "../context";
